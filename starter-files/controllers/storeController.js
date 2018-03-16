75826923399943b7031be528c7fbc0ce6d158ca9
digaodev <rodrigo.rb.dev@gmail.com>
2018-03-16 19:00:54 -0300

    Feature: Add stores, edit stores and its controller. Add catchErrors to routes.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
@@ -6,6 +10,38 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash(
+    'success',
+    `Successfully created store <strong>${
+      store.name
+    }</strong>. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findById(req.params.id);
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, //returns the updated doc
+    runValidators: true
+  }).exec();
+
+  req.flash(
+    'success',
+    `Successfully updated store <strong>${
+      store.name
+    }</strong>. <a href="/stores/${store.slug}">View Store</a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
