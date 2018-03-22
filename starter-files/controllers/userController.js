c3a99fb155100715de80602336a653fe9aca58a7
digaodev <rodrigo.rb.dev@gmail.com>
2018-03-22 09:55:11 -0300

    Feature: add account feature, its routes and reset password functionality

--- starter-files/controllers/userController.js
+++ starter-files/controllers/userController.js
@@ -58,3 +58,23 @@ exports.register = async (req, res, next) => {
 
   next();
 };
+
+exports.account = (req, res) => {
+  res.render('account', { title: 'Edit Your Account' });
+};
+
+exports.updateAccount = async (req, res) => {
+  const updates = {
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: updates },
+    { new: true, runValidators: true, context: 'query' }
+  );
+
+  req.flash('success', 'Profile was updated');
+  res.redirect('back');
+};
