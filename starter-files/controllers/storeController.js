16eae1bedb6a61a555c965ff47d1807da1bbf941
digaodev <rodrigo.rb.dev@gmail.com>
2018-03-30 19:50:37 -0300

    Fix: change path to store photo in store form. Feature: add logic to handle store edit only by its author

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -40,6 +40,8 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
+  req.body.author = req.user._id;
+
   const store = await new Store(req.body).save();
   req.flash(
     'success',
@@ -55,8 +57,17 @@ exports.getStores = async (req, res) => {
   res.render('stores', { title: 'Stores', stores });
 };
 
+const confirmOwner = (store, user) => {
+  if (!user || !store.author.equals(user._id)) {
+    throw Error('You must own the store in order to edit it');
+  }
+};
+
 exports.editStore = async (req, res) => {
   const store = await Store.findById(req.params.id);
+
+  confirmOwner(store, req.user);
+
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
 
@@ -78,7 +89,9 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug });
+  const store = await Store.findOne({ slug: req.params.slug }).populate(
+    'author'
+  );
   if (!store) return next();
   res.render('store', { store, title: store.name });
 };
